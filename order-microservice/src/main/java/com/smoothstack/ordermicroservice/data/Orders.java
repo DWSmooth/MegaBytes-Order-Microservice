216aa077de6e1fbee3d4d981bd1f7d83d6bc6eb4
Per Van Dyke <per.vandyke@smoothstack.com>
2022-05-10 15:54:37 -0400

    added orders file

---
+++ order-microservice/src/main/java/com/smoothstack/ordermicroservice/data/Orders.java
@@ -0,0 +1,12 @@
+package com.smoothstack.ordermicroservice.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Orders {
+    
+}
