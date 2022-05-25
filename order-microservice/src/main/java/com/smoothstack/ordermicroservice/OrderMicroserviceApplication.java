3976829f817ecbd5c12aa7657929599b6478b67b
Sinblio-SmoothStack <benls@iastate.edu>
2022-05-25 14:35:54 -0500

    Configured for eureka and gateway

--- order-microservice/src/main/java/com/smoothstack/ordermicroservice/OrderMicroserviceApplication.java
+++ order-microservice/src/main/java/com/smoothstack/ordermicroservice/OrderMicroserviceApplication.java
@@ -3,6 +3,7 @@ package com.smoothstack.ordermicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.smoothstack")
 @EnableJpaRepositories("com.smoothstack")
 @SpringBootApplication()
+@EnableEurekaClient
 public class OrderMicroserviceApplication {
 
 	public static void main(String[] args) {
